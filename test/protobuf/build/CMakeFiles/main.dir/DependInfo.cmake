42f1835eb2a1379f07d3670ddf7f7fde969fb2a0
doggdong <1126976018@qq.com>
2023-08-30 20:50:21 +0800

    test proto

---
+++ test/protobuf/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dog/Documents/code/cpp_learning/mprpc/myRPC/test/protobuf/main.cc" "CMakeFiles/main.dir/main.cc.o" "gcc" "CMakeFiles/main.dir/main.cc.o.d"
+  "/home/dog/Documents/code/cpp_learning/mprpc/myRPC/test/protobuf/test.pb.cc" "CMakeFiles/main.dir/test.pb.cc.o" "gcc" "CMakeFiles/main.dir/test.pb.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
